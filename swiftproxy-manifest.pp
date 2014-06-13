be4ec4ff945073f5a05954e2a87f8b50142b28b8
1overcosc <scotthshorkey@gmail.com>
2014-06-13 11:39:46 -0400

    commit3

---
+++ swiftproxy-manifest.pp
@@ -0,0 +1,14 @@
+#Manifest to configure a node to act as a swift proxy server
+#These are things that have to be done on the host node BEFORE the swift controller Docker container can be started up!
+
+#Set ip address in proxy file
+file_line { 'ipconfig:':
+	path=>"/opt/biocloud/docker/proxy/proxy-server.conf",
+	line=>"bind_ip = ${ipaddress_eth0}",
+	match=>".*bind_ip.*",
+}
+
+#Ensure memcached is installed--needs to be installed on host OS for docker to work properly with swift
+package { 'memcached':
+	ensure=>latest,
+}
